6b4e17775115d780e64cc026408e09893ff85be5
plkyue <59988663+plkyue@users.noreply.github.com>
2021-02-26 20:22:07 -0500

    forEach replacing for loop in js file

--- JS/script.js
+++ JS/script.js
@@ -2,24 +2,46 @@ let pokemonList = [
     {
         name: 'Squirtle',
         height: 51,
-        types: 'water'
+        types: 'Water'
     },
     {
         name: 'Charizard',
         height: 150,
-        types: ['fire', 'flying' ]
+        types: ['Fire', 'Flying' ]
     },
     {
         name: 'Ivysaur',
         height: 99,
-        types: ['grass', 'poison']
+        types: ['Grass', 'Poison']
     }
-]
+]   
 
-for(let i=0; pokemonList[i]; i++){
-    document.write(`${pokemonList[i].name} (height:${pokemonList[i].height}cm) `);
-    if(pokemonList[i].height > 100){
-        document.write(`Wow, that's a big Pokemon! `)
-    }
-    document.write("<br><br>");
+// for(let i=0; pokemonList.length; i++){
+//     Object.keys(pokemonList[i]).forEach(function(property){
+//         document.write(pokemonList[i][property] + " ")
+//     });
+//     document.write("<br>");
+// }
+
+for(let i=0; pokemonList.length; i++){
+    Object.keys(pokemonList[i]).forEach(function(property){
+        document.write(pokemonList[i][property] + " ")
+    });
+    document.write("<br>");
 }
+
+
+
+
+
+
+// for(let i=0; i < pokemonList.length; i++) {
+//     document.querySelector('[class=pokemon-'+CSS.escape(i)+'-name]').innerText = pokemonList[i].name;
+//     if(pokemonList[i].height >= 100){
+//         document.querySelector('[class=pokemon-'+CSS.escape(i)+'-description]').innerText = 
+//         "Height:"+pokemonList[i].height+"cm and Type:"+ pokemonList[i].types + "\n" + "** Wow, That's a big Pokemon! **";
+//     } else {
+//         document.querySelector('[class=pokemon-'+CSS.escape(i)+'-description]').innerText = 
+//         "Height:"+pokemonList[i].height+"cm and Type:"+pokemonList[i].types;
+//     }
+// }
