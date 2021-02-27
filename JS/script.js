06a27a5c674136528e6f823241b19d3e74b4f7a4
plkyue <59988663+plkyue@users.noreply.github.com>
2021-02-27 01:17:11 -0500

    added IIFE & functions as keys inside IIFE in js file

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,51 @@
-let pokemonList = [
-    {
-        name: 'Squirtle',
-        height: 51,
-        types: 'Water'
-    },
-    {
-        name: 'Charizard',
-        height: 150,
-        types: ['Fire', 'Flying' ]
-    },
-    {
-        name: 'Ivysaur',
-        height: 99,
-        types: ['Grass', 'Poison']
-    }
-]   
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Squirtle',
+            height: 51,
+            types: ['Water']
+        },
+        {
+            name: 'Charizard',
+            height: 150,
+            types: ['Fire', 'Flying' ]
+        },
+        {
+            name: 'Ivysaur',
+            height: 99,
+            types: ['Grass', 'Poison']
+        }
+    ]
+    
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    };
 
-// for(let i=0; pokemonList.length; i++){
-//     Object.keys(pokemonList[i]).forEach(function(property){
-//         document.write(pokemonList[i][property] + " ")
+    function getAll() {
+        return pokemonList;
+    };
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+pokemonRepository.getAll().forEach((pokemon) => {
+    document.write('Name:' + pokemon.name + ' Height:' + pokemon.height + 'cm Type:' + pokemon.types + '<br>');
+});
+
+
+
+
+
+// for(let i=0; pokemonRepository.getAll().length; i++){
+//     Object.keys(pokemonRepository.getAll()[i]).forEach(function(property){
+//         document.write((pokemonRepository.getAll()[i]) + " ")
 //     });
 //     document.write("<br>");
 // }
-
-for(let i=0; pokemonList.length; i++){
-    Object.keys(pokemonList[i]).forEach(function(property){
-        document.write(pokemonList[i][property] + " ")
-    });
-    document.write("<br>");
-}
-
-
-
-
 
 
 // for(let i=0; i < pokemonList.length; i++) {
